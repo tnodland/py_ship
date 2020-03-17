eab8156b4dbbea29c16fc246a25ebee3b7f83c87
Trevor Nodland <trevor.nodland@gmail.com>
2020-03-17 14:43:09 -0600

    adds test for cell attributes

--- cell_test.py
+++ cell_test.py
@@ -4,7 +4,11 @@ from lib.cell import Cell
 
 class CellTest(unittest.TestCase):
     def test_cell_attributes(self):
+        cell = Cell("B4")
 
+        assertEqual("B4", cell.coordinate)
+        assertEqual(null, cell.ship)
+        assertEqual(True, cell.empty)
 
 if __name__ == '__main__':
     unittest.main()
