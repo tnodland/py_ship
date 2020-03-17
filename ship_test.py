a5d5278b4540a3ab2f1a4b6a088f581d63d9020a
Trevor Nodland <trevor.nodland@gmail.com>
2020-03-17 12:49:06 -0600

    adds test for the hit method

--- ship_test.py
+++ ship_test.py
@@ -9,6 +9,12 @@ class ShipTest(unittest.TestCase):
         self.assertEqual(3, cruiser.length)
         self.assertEqual(3, cruiser.health)
 
+    def test_ship_can_be_hit(self):
+        cruiser = Ship("Cruiser", 3)
+
+        self.assertEqual(cruiser.health, 3)
+        cruiser.hit
+        self.assertEqual(cruiser.health, 2)
 
 if __name__ == '__main__':
     unittest.main()
