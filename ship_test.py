4e79c481c72fcf66ec3f70a5331a60ce35064baa
Trevor Nodland <trevor.nodland@gmail.com>
2020-03-17 12:24:01 -0600

    renames attribute test, finishes testing both attributes

--- ship_test.py
+++ ship_test.py
@@ -5,9 +5,10 @@ class ShipTest(unittest.TestCase):
     # def setUp(self):
     #     cruiser = Ship("Cruiser", 3)
 
-    def test_name(self):
+    def ship_attributes(self):
         cruiser = Ship("Cruiser", 3)
         self.assertEqual("Cruiser", cruiser.name)
+        self.assertEqual(3, cruiser.length)
 
 if __name__ == '__main__':
     unittest.main()
