b501b8fe09ea3976081d6eb6ffac440ddd33667a
Trevor Nodland <trevor.nodland@gmail.com>
2020-03-17 12:39:38 -0600

    adds test_ to test name in order to make tests run properly, removes unused set_up

--- ship_test.py
+++ ship_test.py
@@ -2,13 +2,13 @@ import unittest
 from lib.ship import Ship
 
 class ShipTest(unittest.TestCase):
-    # def setUp(self):
-    #     cruiser = Ship("Cruiser", 3)
-
-    def ship_attributes(self):
+    def test_ship_attributes(self):
         cruiser = Ship("Cruiser", 3)
+
         self.assertEqual("Cruiser", cruiser.name)
         self.assertEqual(3, cruiser.length)
+        
+
 
 if __name__ == '__main__':
     unittest.main()
