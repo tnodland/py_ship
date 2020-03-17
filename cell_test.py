8b5f7718c976998a58d4f274d4f84926f59c375d
Trevor Nodland <trevor.nodland@gmail.com>
2020-03-17 14:40:28 -0600

    adds shell for cell class and cell test class'

---
+++ cell_test.py
@@ -0,0 +1,10 @@
+import unittest
+from lib.ship import Ship
+from lib.cell import Cell
+
+class CellTest(unittest.TestCase):
+    def test_cell_attributes(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
