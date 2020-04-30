a96217ea2b12c8a34678e9a6d3e15564fba58a59
Trevor Nodland <trevor.nodland@gmail.com>
2020-04-29 22:23:52 -0600

    adds dictionary for cells on the board, adding dynamic board size to 'todo'

--- lib/board.py
+++ lib/board.py
@@ -4,3 +4,21 @@ from lib.cell import Cell
 class Board():
     def __init__self():
         self.exists = True
+        self.cells = {
+            "A1": cell("A1"),
+            "A2": cell("A2"),
+            "A3": cell("A3"),
+            "A4": cell("A4"),
+            "B1": cell("B1"),
+            "B2": cell("B2"),
+            "B3": cell("B3"),
+            "B4": cell("B4"),
+            "C1": cell("C1"),
+            "C2": cell("C2"),
+            "C3": cell("C3"),
+            "C4": cell("C4"),
+            "D1": cell("D1"),
+            "D2": cell("D2"),
+            "D3": cell("D3"),
+            "D4": cell("D4"B
+        }
